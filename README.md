e57853813cb8556d2f2b52db800db4f451898ccd
Christopher Lillthors <lillt@kth.se>
2014-08-23 20:09:10 +0200

    Added install script and some stuff in README.md

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-progp
+progp14
 =====
 
-Lab assignments progp
+Lab assignments progp14
+
+To install this software, see install.sh
+
+LICENSE: MIT
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# In order to run this software you will need Haskell platform and cabal installed on your system.
+
+cabal install bed-and-breakfast
